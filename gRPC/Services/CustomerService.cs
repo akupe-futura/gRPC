d0f5d4665f198020c04814199b306a116413137b
a.kupe <a.kupe@futura-solutions.de>
2022-10-24 16:38:54 +0200

    getting started project for gRPC

---
+++ gRPC/Services/CustomerService.cs
@@ -0,0 +1,70 @@
+﻿using Grpc.Core;
+
+namespace gRPC.Services;
+
+public class CustomerService: Customer.CustomerBase
+{
+    private readonly ILogger<CustomerService> _logger;
+
+    public CustomerService(ILogger<CustomerService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+    {
+        CustomerModel output = new CustomerModel();
+
+        if (request.UserId == 1)
+        {
+            output.FirstName = "Arkida";
+            output.LastName = "Kupe";
+        }
+        else if (request.UserId == 2)
+        {
+            output.FirstName = "Futura";
+            output.LastName = "Solutions";
+        }
+        else
+        {
+            output.FirstName = "John";
+            output.LastName = "Smith";
+        }
+
+        return Task.FromResult(output);
+    }
+    
+    public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+
+    {
+        List<CustomerModel> customers = new List<CustomerModel>
+        {
+            new CustomerModel
+            {
+                FirstName = "Test1",
+                LastName = "Test12",
+                Age = 23,
+                EmailAddress = "test1@gmail.com"
+            },
+            new CustomerModel
+            {
+                FirstName = "Test2",
+                LastName = "Test22",
+                Age = 23,
+                EmailAddress = "test2@gmail.com"
+            },
+            new CustomerModel
+            {
+                FirstName = "Test3",
+                LastName = "Test32",
+                Age = 23,
+                EmailAddress = "test3@gmail.com"
+            },
+        };
+    
+        foreach (var cust in customers)
+        {
+            await responseStream.WriteAsync(cust);
+        }
+    }
+}
---
+++ gRPCClient/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using gRPC;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+// var input = new HelloRequest {Name = "Arkida"};
+var channel = GrpcChannel.ForAddress("http://localhost:5105");
+// var client = new Greeter.GreeterClient(channel);
+// var reply = await client.SayHelloAsync(input);
+//
+// Console.WriteLine(reply.Message);
+
+var clientRequested = new CustomerLookupModel{UserId = 1};
+var customerClient = new Customer.CustomerClient(channel);
+var customer = await customerClient.GetCustomerInfoAsync(clientRequested);
+
+Console.WriteLine($"{customer.FirstName} {customer.LastName}");
+
+Console.WriteLine();
+Console.WriteLine("New CustomerRequest");
+Console.WriteLine();
+
+using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+{
+    while (await call.ResponseStream.MoveNext())
+    {
+        var currentCustomer = call.ResponseStream.Current;
+        
+        Console.WriteLine($"{ currentCustomer.FirstName } { currentCustomer.LastName }: { currentCustomer.EmailAddress }");
+    }
+}
+
+Console.ReadLine();
